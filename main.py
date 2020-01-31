9dbb2e40352eca175dba86898782f5e22a9a3195
Leonardo Cardoso-Rodriguez <ljcard21>
2020-01-31 10:44:13 -0500

    ,m

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 # A starter program for Python with Tkinter
 
+
 from tkinter import * # import Tkinter library
 window = Tk()         # Create the application window
 
 # Add a label with the text "Hello"
-lbl = Label(window, text="Why hello there. I'm in a programming class.")
-
-# Place the label in the window at 0, 0
+lbl = Label(window, text="Why hello there. I'm in a programming class." , font=("Arial Bold", 30))
 lbl.grid(column=0, row=0)
+
+zxc = Label(window, text="I'm 15 yrs old." , font=("Arial Bold", 38))
+zxc.grid(column=1, row=1)
+
 
 
 window.mainloop()     # Keep the window open
---
+++ warm up.py
@@ -0,0 +1,12 @@
+x = 51
+y = 23
+
+if x > 50:
+  x = 5
+  if y < 20:
+    x = 3
+else:
+  x = 2
+  y = 10
+
+print(x + y)
